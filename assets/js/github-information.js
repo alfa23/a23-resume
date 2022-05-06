c0864f576f8a8286ece3bf3f3e3254cf8ea1e4a1
alfa23 <paw235@gmail.com>
2022-05-06 01:23:16 +0000

    fix: Add js to clear gh-user-data & gh-repo-data elements bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
