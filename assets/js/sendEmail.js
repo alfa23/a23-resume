543f37057d6a345fd8f02ea70c0ccb66969aa6be
alfa23 <paw235@gmail.com>
2022-05-08 03:44:39 +0000

    add: Implement EmailJS integration to contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_icn5r5n", "template_k7op65b", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
